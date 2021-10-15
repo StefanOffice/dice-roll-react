55bc002b6e64bcd1160407675ec1e5dfdc07044f
Stefan Stefanovic <stefan.stefanovic.office@gmail.com>
2021-10-15 15:05:17 +0200

    Add a single Die component

---
+++ src/Die.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Die.css';
+
+class Die extends Component {
+    render(){
+        const { face } = this.props;
+        return (
+            <i class={`Die fas fa-dice-${face}`}></i>
+        )
+    }
+}
+
+export default Die;
